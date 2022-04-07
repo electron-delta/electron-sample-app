b068af7c8bf6a8b5b267cbacbbc58637cf811e3a
sandeep1995 <i.am.sandeep.acharya@gmail.com>
2022-04-07 20:46:08 +0530

    use correct repo

--- afterAllArtifactBuild.js
+++ afterAllArtifactBuild.js
@@ -9,9 +9,7 @@ const delay = (ms) =>
 
 exports.default = async function (context) {
   console.log("after all Artifact Build.js", context);
-  await delay(10000);
-  console.log("yo 10 second later");
-  await delay(50000);
-  console.log("yo 50 seconds later");
+  await delay(1000);
+  console.log("yo 1 second later");
   return [path.resolve(__dirname, "./afterAllArtifactBuild.js")];
 };
