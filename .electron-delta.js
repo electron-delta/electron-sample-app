924290c286313261a3c09cd591823f9d99f8cddf
sandeep1995 <i.am.sandeep.acharya@gmail.com>
2022-04-08 17:35:34 +0530

    swiftly integrated

--- .electron-delta.js
+++ .electron-delta.js
@@ -1,6 +1,31 @@
-module.exports = {
-  getLastReleaseDownloadURLs: async () => {
-    return [];
+// .electron-delta.js
+const DeltaBuilder = require("@electron-delta/builder");
+const path = require("path");
+
+const options = {
+  productIconPath: path.join(__dirname, "icon.ico"),
+  productName: "electron-quick-start",
+  processName: "electron-quick-start",
+
+  cache: path.join(__dirname, ".cache/ELECTRON_DELTA"),
+  logger: console,
+
+  getPreviousReleases: async () => {
+    return [
+      "https://github.com/electron-delta/electron-sample-app/releases/download/v0.0.6/electron-quick-start-0.0.6.exe",
+      "https://github.com/electron-delta/electron-sample-app/releases/download/v0.0.4/electron-quick-start-0.0.4.exe",
+      "https://github.com/electron-delta/electron-sample-app/releases/download/v0.0.3/electron-quick-start-0.0.3.exe",
+    ];
   },
-  sign: async (filePath) => {},
+  sign: async (filePath) => {
+    return filePath;
+  },
+};
+
+exports.default = async function (context) {
+  const deltaInstallerFiles = await DeltaBuilder.build({
+    context,
+    options,
+  });
+  return deltaInstallerFiles;
 };
