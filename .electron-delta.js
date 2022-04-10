cac95fe33706d384e18e3aa5f5ab58f17a44e70b
sandeep1995 <i.am.sandeep.acharya@gmail.com>
2022-04-10 15:30:55 +0530

    no releases

--- .electron-delta.js
+++ .electron-delta.js
@@ -8,10 +8,18 @@ const options = {
   productName: "electron-sample-app",
 
   getPreviousReleases: async () => {
-    return [
-      "https://github.com/electron-delta/electron-sample-app/releases/download/v0.0.1/electron-sample-app-0.0.1.exe",
-      "https://github.com/electron-delta/electron-sample-app/releases/download/v0.0.2/electron-sample-app-0.0.2.exe",
-    ];
+    let releases = await fetch(
+      "https://api.github.com/repos/electron-delta/electron-sample-app/releases"
+    ).then((res) => res.json());
+
+    console.log(releases);
+
+    return [];
+
+    // return [
+    //   "https://github.com/electron-delta/electron-sample-app/releases/download/v0.0.1/electron-sample-app-0.0.1.exe",
+    //   "https://github.com/electron-delta/electron-sample-app/releases/download/v0.0.2/electron-sample-app-0.0.2.exe",
+    // ];
   },
   sign: async (filePath) => {
     // sign each delta executable
