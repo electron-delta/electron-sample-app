6672fabc2f7220a21143240ebacad665a6a36744
sandeep1995 <i.am.sandeep.acharya@gmail.com>
2022-04-10 13:57:13 +0530

    connect appveyor.yml

---
+++ change-verison.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const fs = require("fs");
+
+const modifyPackageJsonVersion = (newVersion) => {
+  const packageJsonPath = path.resolve(__dirname, "package.json");
+  const packageJsonContents = fs.readFileSync(packageJsonPath);
+  const packageJson = JSON.parse(packageJsonContents);
+
+  const oldVersion = packageJson.version;
+  packageJson.version = newVersion;
+
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
+  console.log(
+    `package.json version changed from ${oldVersion} to ${newVersion}`
+  );
+};
+
+try {
+  let newVersion = process.env.APPVEYOR_BUILD_VERSION;
+  modifyPackageJsonVersion(newVersion);
+} catch (err) {
+  console.error(err);
+}
