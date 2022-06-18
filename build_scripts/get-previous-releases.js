1960ed3dedd7d22580375610f323cb99c9573b16
sandeep1995 <i.am.sandeep.acharya@gmail.com>
2022-06-19 03:19:53 +0530

    second attempt

--- build_scripts/get-previous-releases.js
+++ build_scripts/get-previous-releases.js
@@ -1,24 +1,23 @@
 const axios = require("axios").default;
 
-const headers = {
-  "Authorization": `token ${process.env.GH_TOKEN}`,
-};
+// const headers = {
+//   "Authorization": `token ${process.env.GH_TOKEN}`,
+// };
 
 const getPreviousReleases = async ({ platform, target }) => {
   let { data } = await axios.get(
     "https://api.github.com/repos/electron-delta/electron-sample-app/releases",
-    {
-      headers,
-    }
   );
+
+  const ext = platform === 'win' ? (target === 'nsis-web' ? ".7z" : ".exe") : ".zip";
   let prevReleases = data.reduce((arr, release) => {
     release.assets
       .map((d) => {
         return d.browser_download_url
       })
-      .filter((d) => d.endsWith(platform === 'win' ? (target === 'nsis-web' ? ".7z" : ".exe") : ".zip"))
+      .filter((d) => d.endsWith(ext))
       .forEach((url) => {
-        if (!url.includes("delta")) {
+        if (!url.endsWith("-delta.exe")) {
           arr.push({ version: release.tag_name, url });
         }
       });
